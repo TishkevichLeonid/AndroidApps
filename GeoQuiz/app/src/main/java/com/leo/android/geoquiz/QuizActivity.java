7342af02ab3e38ef6fb6f8c96bb98f8f646f7d4a
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-02-02 14:50:56 +0300

    GeoQuiz release

--- GeoQuiz/app/src/main/java/com/leo/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/leo/android/geoquiz/QuizActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -16,13 +17,24 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        mFalseButtom = (Button) findViewById(R.id.false_button);
         mTrueButtom = (Button) findViewById(R.id.true_button);
         mTrueButtom.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
 
             }
         });
+
+        mFalseButtom = (Button) findViewById(R.id.false_button);
+        mFalseButtom.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
     }
 
     @Override
