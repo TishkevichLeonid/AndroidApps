c6806a00eaee60608415802fdf398fd1b69fdfbc
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-02-04 23:05:19 +0300

    GeoQuiz add questions1

--- GeoQuiz/app/src/main/java/com/leo/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/leo/android/geoquiz/QuizActivity.java
@@ -5,28 +5,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private Button mTrueButtom;
-    private Button mFalseButtom;
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private int mCurrentIndex = 0;
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        mTrueButtom = (Button) findViewById(R.id.true_button);
-        mTrueButtom.setOnClickListener(new View.OnClickListener(){
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view); // присвоили переменной TextView id виджета Text view
+        int question = mQuestionBank[mCurrentIndex].getTextResId(); // присваиваем id вопроса из массива Question переменной question
+        mQuestionTextView.setText(question); // вызываем метод setText класса TextView передавая в него id вопроса
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
 
             }
         });
 
-        mFalseButtom = (Button) findViewById(R.id.false_button);
-        mFalseButtom.setOnClickListener(new View.OnClickListener(){
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
 
